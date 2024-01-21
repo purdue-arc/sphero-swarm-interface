14258c500fc505df6041e3fede1d3858419684c8
deepanshsaxena <deepanshs@live.com>
2024-01-21 17:45:08 -0500

    deepansh

---
+++ lib/screens/moleculesList.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class MoleculesList extends StatefulWidget {
+  const MoleculesList({super.key});
+
+  @override
+  State<MoleculesList> createState() => _MoleculesListState();
+}
+
+class _MoleculesListState extends State<MoleculesList>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+  
+}
+
